db0ff7994ffbaae696e0fa00401050249bb9a6fe
Bill <ceriustech@gmail.com>
2020-05-16 16:54:06 -0500

    Initial commit for cra test app.

---
+++ src/comnponents/card/card.component.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+import './card.styles.css';
+
+const Card = (props) => {
+    return (
+        <div className="card-container">
+            <img 
+                alt="monster" 
+                src={`https://robohash.org/${props.monster.id}?set=set2&size=180x180`} 
+                style={{width: '15em'}}
+            
+            />
+            <h2>{props.monster.name}</h2>
+            <p>{props.monster.email}</p>
+        </div>
+    ); 
+}
+
+export default Card; 
